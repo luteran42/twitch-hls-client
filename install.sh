a908c510aaa34a7d0ab76cc302c85fe93c38ca32
Luterán Lajos <luteranlajos@protonmail.ch>
2024-02-14 15:03:44 +0100

    upd install.sh
    Signed-off-by: Luterán Lajos <luteranlajos@protonmail.ch>

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
-cargo zigbuild --target x86_64-unknown-linux-musl --release
-cp -fv ./target/x86_64-unknown-linux-musl/release/twitch-hls-client ~/.local/bin/cargo/bin/
+
+## musl build
+# cargo zigbuild --target x86_64-unknown-linux-musl --release
+# cp -fv ./target/x86_64-unknown-linux-musl/release/twitch-hls-client ~/.local/bin/cargo/bin/
+
+## normal build
 # cargo b --release
 # cp -fv ./target/release/twitch-hls-client ~/.local/bin/cargo/bin/
+
+## std build
+cargo build -Z build-std=std,panic_abort -Z build-std-features= --release --target x86_64-unknown-linux-gnu
+cp -fv ./target/x86_64-unknown-linux-gnu/release/twitch-hls-client ~/.local/bin/cargo/bin/
